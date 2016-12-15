40cb61531eb30bc51be506f05f23422cf20ba03d
jaouad <jaouadtelmoudy@gmail.com>
2016-12-15 11:03:06 +0100

    Branche2 : langues.component & contact-component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core'
 import { RouterModule } from '@angular/router';
 import { rootRouterConfig } from './app.routes';
 import { AppComponent } from './app.component';
-import { GithubService } from './github/shared/github.service';
+
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpModule } from '@angular/http';
@@ -11,29 +11,27 @@ import { FormationComponent } from './formation/formation.component';
 import { ExperienceComponent } from './experience/experience.component';
 import { ProjetComponent } from './projet/projet.component';
 import { HomeComponent } from './home/home.component';
-import { RepoBrowserComponent } from './github/repo-browser/repo-browser.component';
-import { RepoListComponent } from './github/repo-list/repo-list.component';
-import { RepoDetailComponent } from './github/repo-detail/repo-detail.component';
+
 import { LocationStrategy, HashLocationStrategy } from '@angular/common';
 import { ContactComponent } from './contact/contact.component';
 import { MenuComponent } from './menu/menu.component';
 import { FooterComponent } from './footer/footer.component';
 import {LoisirsComponent} from "./loisirs/loisirs.component";
+import {LanguesComponent} from "./langues/langues.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     FormationComponent,
-    RepoBrowserComponent,
-    RepoListComponent,
-    RepoDetailComponent,
+  
     HomeComponent,
     ContactComponent,
     MenuComponent,
     FooterComponent,
     ExperienceComponent,
     ProjetComponent,
-    LoisirsComponent
+    LoisirsComponent,
+    LanguesComponent
   ],
   imports: [
     BrowserModule,
@@ -43,7 +41,7 @@ import {LoisirsComponent} from "./loisirs/loisirs.component";
     RouterModule.forRoot(rootRouterConfig, { useHash: true })
   ],
   providers: [
-    GithubService
+
   ],
   bootstrap: [ AppComponent ]
 })
---
+++ src/app/langues/langues.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+@Component({
+    selector:'langues',
+    styleUrls:['./langues.component.css'],
+    templateUrl:'./langues.component.html'
+})
+
+export class LanguesComponent{
+
+}
